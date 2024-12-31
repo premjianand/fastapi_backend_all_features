f5ff8dec5b214d8dec7f835d51eca7daf6f15daa
pream9413 <pream@innoboon.com>
2024-12-31 09:46:05 +0530

    including port.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 
 from app.schemas import USERDETAILS
@@ -8,3 +9,10 @@ app = FastAPI()
 async def createuser(userdetails:USERDETAILS):
     response = f"User has been sucessfully created. {userdetails.USERNAME}"
     return response
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Use the PORT environment variable or default to 8000
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
