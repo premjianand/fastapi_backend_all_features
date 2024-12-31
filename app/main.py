a3912450c7b8c0be7ff192bb3827bec9c81f6f92
pream9413 <pream@innoboon.com>
2024-12-31 17:49:51 +0530

    testing without specifying port and other stuff.

--- app/main.py
+++ app/main.py
@@ -5,22 +5,22 @@ from app.schemas import USERDETAILS
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "FastAPI application is running!"}
+# @app.get("/")
+# def read_root():
+#     return {"message": "FastAPI application is running XOXO !"}
 
-@app.head("/")
-def health_check():
-    return {}
+# @app.head("/")
+# def health_check():
+#     return {}
 
 @app.post('/Create_User')
 async def createuser(userdetails:USERDETAILS):
     response = {"message":f"User has been sucessfully created. {userdetails.USERNAME}"}
     return response
 
-if __name__ == "__main__":
-    import uvicorn
+# if __name__ == "__main__":
+#     import uvicorn
 
-    # Use the PORT environment variable or default to 8000
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+#     # Use the PORT environment variable or default to 8000
+#     port = int(os.getenv("PORT", 8000))
+#     uvicorn.run(app, host="0.0.0.0", port=port)
