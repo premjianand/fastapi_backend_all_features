c6288d531e5b8f6de9abecba4e9d293b774fee94
pream9413 <pream@innoboon.com>
2024-12-31 13:14:30 +0530

    adding route "/"

--- app/main.py
+++ app/main.py
@@ -5,9 +5,13 @@ from app.schemas import USERDETAILS
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI application is running!"}
+
 @app.post('/Create_User')
 async def createuser(userdetails:USERDETAILS):
-    response = f"User has been sucessfully created. {userdetails.USERNAME}"
+    response = {"message":f"User has been sucessfully created. {userdetails.USERNAME}"}
     return response
 
 if __name__ == "__main__":
