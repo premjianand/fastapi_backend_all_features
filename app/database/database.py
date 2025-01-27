df3e330aaf4a766a773f9b24ead6b4d6536abeac
pream9413 <pream@innoboon.com>
2025-01-27 23:27:23 +0530

    Creating DB connection and users table.

---
+++ app/database/database.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker,scoped_session
+
+from app.config import config
+
+username = config["DATABASE"]["USERNAME"]
+password = config["DATABASE"]["PASSWORD"]
+database = config["DATABASE"]["DATABASE"]
+# conn_str = config["DATABASE"][]
+port = config["DATABASE"]["PORT"]
+host = config["DATABASE"]["HOST"]
+
+print(username,password,database,port,host)
+db_string = f'postgresql://{username}:{password}@{host}:{port}/{database}'
+
+engine = create_engine(db_string, pool_size=20, max_overflow=0)
+
+engine = create_engine(db_string, pool_size=20, max_overflow=0)
+
+Base = declarative_base()
+session = scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+def get_db():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.autoflush = False
+
+    try:
+        yield session
+    except Exception as e:
+        raise e
+    finally:
+        session.rollback()
+        session.close()
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,17 @@
 from pydantic import BaseModel
+from enum import Enum
+
+
+class ROLE_ENUM(str,Enum):
+    ADMIN="ADMIN"
+    USER="USER"
+
 
 class USERDETAILS(BaseModel):
     USERNAME:str
-    EMAILID:str
+    EMAIL_ID:str
     PASSWORD:str
+    FIRST_NAME:str
+    LAST_NAME:str
+    PHONE_NUMBER:int
+    ROLE:ROLE_ENUM
