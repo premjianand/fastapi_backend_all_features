c6b828bff912697f02f787bd3a3df76e19190f56
pream9413 <pream@innoboon.com>
2025-01-30 00:54:54 +0530

    code for password encryption.

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-
 from sqlalchemy import Integer,String,Column,Boolean,DateTime,Enum
+from sqlalchemy.types import BINARY
 
 from app.database.database import Base
 from app.schemas import ROLE_ENUM
@@ -9,13 +8,13 @@ class Users(Base):
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
 
-    user_id = Column(Integer, primary_key=True, autoincrement='ignore_fk')
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String)
     first_name = Column(String)
     last_name = Column(String)
-    password = Column(String)
+    password = Column(BINARY)
     email_id = Column(String)
-    phone_number = Column(Integer)
+    phone_number = Column(String)
     role = Column(Enum(ROLE_ENUM))
     created_ts = Column(DateTime)
     updated_ts = Column(DateTime)
